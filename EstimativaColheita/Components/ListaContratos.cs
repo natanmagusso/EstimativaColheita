05c7f4564dac2c88ebaabdc176ae64ad4c5d4d9a
Natan Piassi Magusso <natanmagusso@hotmail.com>
2022-05-23 16:22:20 -0300

    Create Components

---
+++ EstimativaColheita/Components/ListaContratos.cs
@@ -0,0 +1,22 @@
+﻿using EstimativaColheita.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstimativaColheita.Components
+{
+    public class ListaContratos : ViewComponent
+    {
+        private readonly IContrato _contrato;
+
+        public ListaContratos(IContrato contrato)
+        {
+            _contrato = contrato;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var contratos = _contrato.ConsultarContratosAtivosAsync();
+
+            return View(contratos);
+        }
+    }
+}

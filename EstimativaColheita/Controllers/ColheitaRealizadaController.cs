57904ebd174de7086ca9db6c81645febf11703de
Natan Piassi Magusso <natanmagusso@hotmail.com>
2022-05-26 08:49:31 -0300

    Alter Controllers

--- EstimativaColheita/Controllers/ColheitaRealizadaController.cs
+++ EstimativaColheita/Controllers/ColheitaRealizadaController.cs
@@ -28,6 +28,7 @@ namespace EstimativaColheita.Controllers
         public IActionResult Create()
         {
             ViewData["IdContrato"] = new SelectList(_contrato.ConsultarContratosAtivosAsync(), "Id", "DescricaoCompleta");
+            ViewData["IdTalhao"] = new SelectList(_talhao.ConsultarTalhoesAtivosAsync(), "Id", "DescricaoCompleta");
             ViewData["IdEncarregado"] = new SelectList(_encarregado.ConsultarEncarregadosAtivosAsync(), "Id", "DescricaoCompleta");
 
             return View(new ColheitaRealizadaModel());
@@ -57,12 +58,6 @@ namespace EstimativaColheita.Controllers
             ViewData["IdContrato"] = new SelectList(_contrato.ConsultarContratosAtivosAsync(), "Id", "DescricaoCompleta", colheitaRealizada.IdContrato);
             ViewData["IdEncarregado"] = new SelectList(_encarregado.ConsultarEncarregadosAtivosAsync(), "Id", "DescricaoCompleta", colheitaRealizada.IdEncarregado);
             return View(colheitaRealizada);
-        }
-        [HttpGet]
-        public IActionResult ListarTalhoesColheitaRealizada(string contrato)
-        {
-            var listaTalhoes = _talhao.ConsultarTalhoesAtivosContratoAsync(Convert.ToInt32(contrato));
-            return new JsonResult( new { resultado = listaTalhoes });
         }
     }
 }
